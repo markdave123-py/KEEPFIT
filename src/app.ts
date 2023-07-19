014aa50240125fdb7ce9d5b62b9ff9c73ef28130
markdave123-py <nwaekwudavid@gmail.com>
2023-07-19 04:22:46 +0100

    KeepFit project

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+// src/app.ts
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});

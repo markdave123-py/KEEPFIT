332940a9dab01c576cad6380fbc52ea701b5f9fd
markdave123-py <nwaekwudavid@gmail.com>
2023-08-01 01:54:55 +0100

    keepfit

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,2 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 import { USER } from 'src/models/user';
+
+
+export const createNewUser = async (req: Request, res:Response, next: NextFunction) =>{
+
+    const { firstName, lastName, } = req.body;
+
+}

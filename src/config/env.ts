130fd40a84023df8d9de634e06e8f5c9c4f0e95a
markdave123-py <nwaekwudavid@gmail.com>
2023-08-18 02:05:44 +0100

    keeping fit

--- src/config/env.ts
+++ src/config/env.ts
@@ -8,6 +8,8 @@ export const config = Object.freeze(
     {
     port : process.env.PORT,
     mongoUrl: process.env.MONGO_URL as string,
-    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET
+    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET as string,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET as string
 }
 )
--- src/services/passport-setup.ts
+++ src/services/passport-setup.ts
@@ -1,17 +1,29 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import { config } from 'src/config/env';
 
 
-passport.use( new GoogleStrategy({
-    clientID: '//somethoing',
-    clientSecret: '',
-    callbackURL: '',
-    passReqToCallback: ''
+passport.use ( 
+    new GoogleStrategy(
+        {
+    clientID: config.GOOGLE_CLIENT_ID,
+    clientSecret: config.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3400/auth/google/callback",
+    passReqToCallback: true
 
     
-    }), () => {
+    }, (req, accessToken, refreshToken, profile, done) => {
+         done (null, profile);
 
     }
-    
-    
-)
+   
+    )   
+);
+
+passport.serializeUser((user, done) =>{
+    done(null, user)
+});
+
+passport.deserializeUser((user:any, done) =>{
+    done(null, user)
+});
